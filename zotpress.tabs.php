65a828025db4ca7245beb1a470b489073a2907b6
kseaborn <kseaborn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-13 05:48:29 +0000

    Version 2.0 of Zotpress released. Major changes, all for the good!
    git-svn-id: https://plugins.svn.wordpress.org/zotpress/tags/2.0@344720 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ zotpress.tabs.php
@@ -0,0 +1,18 @@
+<?php
+
+    if (isset($_GET['accounts']) && $_GET['accounts'] == "true")
+        $tagpage = "accounts";
+    else if (isset($_GET['help']) && $_GET['help'] == "true")
+        $tagpage = "help";
+    else
+        $tagpage = "default";
+
+?>
+
+<div id="icon-themes" class="icon32"><br /></div>
+
+<h2 class="nav-tab-wrapper">
+    <a class="nav-tab <?php if ($tagpage == "default") echo "nav-tab-active"; ?>" href="admin.php?page=Zotpress&amp;display=true">Citations</a>
+    <a class="nav-tab <?php if ($tagpage == "accounts") echo "nav-tab-active"; ?>" href="admin.php?page=Zotpress&amp;accounts=true">Zotero Accounts</a>
+    <a class="nav-tab <?php if ($tagpage == "help") echo "nav-tab-active"; ?>" href="admin.php?page=Zotpress&amp;help=true">Help</a>
+</h2>
