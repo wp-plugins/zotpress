ec4c0e0a90b7f7e53c591b415b4c8932a867395d
kseaborn <kseaborn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-13 07:03:13 +0000

    Version 2.1 of Zotpress released. Now cURL-friendly again.
    git-svn-id: https://plugins.svn.wordpress.org/zotpress/tags/2.1@344721 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ zotpress.rss.php
@@ -0,0 +1,169 @@
+<?php
+
+	// Include WordPress
+	require('../../../wp-load.php');
+	define('WP_USE_THEMES', false);
+
+	// Include Special cURL
+	require('zotpress.curl.php');
+	
+	$xml = "";
+
+	if (isset($_GET['account_type']) && isset($_GET['api_user_id']))
+	{
+		
+		// IMAGES
+		
+		if (isset($_GET['displayImages']) && $_GET['displayImages'] == "true")
+		{
+			header('Content-Type: text/xml');
+			
+			$xml = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
+			
+			$image_xml = "";
+			
+			global $wpdb;
+			
+			if (isset($_GET['displayImageByCitationID']))
+				$images = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix."zotpress_images WHERE citation_id='".$_GET['displayImageByCitationID']."'");
+			else
+				$images = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix."zotpress_images");
+			
+			$total = $wpdb->num_rows;
+			
+			foreach ($images as $image)
+				$image_xml .= "	<zpimage citation_id='".$image->citation_id."' account_type='".$image->account_type."' api_user_id='".$image->api_user_id."' image_url='".$image->image."' />\n";
+				
+			$xml .= "\n<zpimages total=\"".$total."\">\n";
+			$xml .= $image_xml;
+			$xml .= "</zpimages>";
+		}
+		else
+		{
+			// ACOUNT TYPE
+			
+			if (isset($_GET['account_type']) && $_GET['account_type'] == "groups")
+				$urlAccountType = "groups";
+			else
+				$urlAccountType = "users";
+			
+			
+			// DATA TYPE
+			
+			if (isset($_GET['data_type']))
+				$urlDataType = trim($_GET['data_type']);
+			else
+				$urlDataType = "items";
+			
+			
+			// LIST
+			
+			if (isset($_GET['collection_id']) && trim($_GET['collection_id']) != '')
+				$urlDataType = "collections/".trim($_GET['collection_id'])."/items";
+			
+			if (isset($_GET['item_key']) && trim($_GET['item_key']) != '')
+				$urlDataType = "items/".trim($_GET['item_key']);
+			
+			if (isset($_GET['tag_name']) && trim($_GET['tag_name']) != '')
+				$urlDataType = "tags/".urlencode(trim($_GET['tag_name']))."/items";
+			
+			
+			// PARAMETERS
+			
+			// Content
+			if (isset($_GET['content']))
+				$content = "&content=" . $_GET['content'];
+			else
+				$content = "&content=bib";
+			
+			// Style
+			if (isset($_GET['style']))
+				$style = "&style=" . trim($_GET['style']);
+			else
+				$style = "&style=apa";
+			
+			// Order
+			if (isset($_GET['order']) && $_GET['order'] != '')
+				$order = "&order=" . $_GET['order'];
+			
+			// Sort
+			if (isset($_GET['sort']) && $_GET['sort'] != '')
+				$sort = "&sort=" . $_GET['sort'];
+			
+			// Limit
+			if (isset($_GET['limit']) && $_GET['limit'] != '')
+				$limit = "&limit=" . $_GET['limit'];
+			
+			// Author
+			if (isset($_GET['author']) && trim($_GET['author'] != ''))
+				$author = trim($_GET['author']);
+				
+				
+			// PUBLIC KEY
+			$zpaccount = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix."zotpress WHERE api_user_id='".trim($_GET['api_user_id'])."'");
+			$public_key = $zpaccount[0]->public_key;
+			
+			// ASSUMED: &format=bib
+			
+			// AUTHOR
+			if (isset($author) && strlen($author) > 0)
+			{
+				if (isset($public_key))
+					$url = "https://api.zotero.org/".$urlAccountType."/".trim($_GET['api_user_id'])."/".$urlDataType."?key=".$public_key."&content=html".$style.$order.$sort;
+				else // GROUP
+					$url = "https://api.zotero.org/".$urlAccountType."/".trim($_GET['api_user_id'])."/".$urlDataType.str_replace("&","?","&content=html").$style.$order.$sort;
+			}
+			
+			// NO AUTHOR
+			else
+			{
+				if (isset($public_key))
+					$url = "https://api.zotero.org/".$urlAccountType."/".trim($_GET['api_user_id'])."/".$urlDataType."?key=".$public_key.$content.$style.$order.$sort.$limit;
+				else // GROUP
+					$url = "https://api.zotero.org/".$urlAccountType."/".trim($_GET['api_user_id'])."/".$urlDataType.str_replace("&","?",$content).$style.$order.$sort.$limit;
+			}
+			
+			
+			// DISPLAY
+			
+			if (isset($_GET['curl']) && trim($_GET['curl']) != "" && trim($_GET['curl']) != "false")
+			{
+				if  (in_array ('curl', get_loaded_extensions()))
+				{
+					$curl = new CURL();
+					$curl->enableCache();
+					$xml = $curl->get($url);
+				}
+				else // Use the regular away anyways
+				{
+					$curl = new CURL();
+					$curl->enableCache();
+					$xml =  $curl->get_file_get_contents($url);
+				}
+			}
+			else
+			{
+				if  (in_array ('curl', get_loaded_extensions()))
+				{
+					$curl = new CURL();
+					$curl->enableCache();
+					$xml = $curl->get($url);
+				}
+				else // Use the regular away
+				{
+					$curl = new CURL();
+					$curl->enableCache();
+					$xml =  $curl->get_file_get_contents($url);
+				}
+			}
+		}
+		
+		
+		// DISPLAY XML
+		
+		print $xml;
+	}
+
+
+
+?>
