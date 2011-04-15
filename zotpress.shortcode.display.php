250c914c5019ccc9b7153ab325166ade78cf0db0
kseaborn <kseaborn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-15 08:54:28 +0000

    Version 3.0. MAJOR RELEASE! OAuth, convenient "Zotpress Reference" meta box, friendly lag handling, numerous bug fixes, and more!
    git-svn-id: https://plugins.svn.wordpress.org/zotpress/tags/3.0@373165 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ zotpress.shortcode.display.php
@@ -0,0 +1,49 @@
+
+
+        <?php
+        
+        $zp_multiple_accounts_accounts = false;
+        
+        if ($GLOBALS['zp_shortcode_attrs']['api_user_id'] !== false || $GLOBALS['zp_shortcode_attrs']['nickname'] !== false)
+            $zp_multiple_accounts = true;
+            
+        if ($zp_multiple_accounts === false) // User id or nickname specified
+        {
+            foreach ($GLOBALS['zp_accounts'] as $account)
+            {
+                if (($GLOBALS['zp_shortcode_attrs']['api_user_id'] !== false && $GLOBALS['zp_shortcode_attrs']['api_user_id'] == $account->api_user_id)
+                        || ($GLOBALS['zp_shortcode_attrs']['nickname'] !== false && $GLOBALS['zp_shortcode_attrs']['nickname'] == $account->nickname))
+                {
+                    echo "DisplayCitations('".$account->account_type."', '".$account->api_user_id."', '".$GLOBALS['zp_shortcode_attrs']['data_type']."', '".$GLOBALS['zp_shortcode_attrs']['collection_id']."', '".$GLOBALS['zp_shortcode_attrs']['item_key']."', '".$GLOBALS['zp_shortcode_attrs']['tag_name']."', '".$GLOBALS['zp_shortcode_attrs']['content']."', '".$GLOBALS['zp_shortcode_attrs']['style']."', '".$GLOBALS['zp_shortcode_attrs']['order']."', '".$GLOBALS['zp_shortcode_attrs']['sort']."', '".$GLOBALS['zp_shortcode_attrs']['year']."', '".$GLOBALS['zp_shortcode_attrs']['download']."', '".$GLOBALS['zp_shortcode_attrs']['author']."', '".$GLOBALS['zp_shortcode_attrs']['limit']."', '".$GLOBALS['zp_shortcode_attrs']['image']."', '".$GLOBALS['zp_instance_id']."', false);\n        ";
+                }
+            }
+        }
+        
+        // Multiple shortcode calls
+        else
+        {
+            foreach ($GLOBALS['zp_accounts'] as $account)
+            {
+                echo "DisplayCitations('".$account->account_type."', '".$account->api_user_id."', '".$GLOBALS['zp_shortcode_attrs']['data_type']."', '".$GLOBALS['zp_shortcode_attrs']['collection_id']."', '".$GLOBALS['zp_shortcode_attrs']['item_key']."', '".$GLOBALS['zp_shortcode_attrs']['tag_name']."', '".$GLOBALS['zp_shortcode_attrs']['content']."', '".$GLOBALS['zp_shortcode_attrs']['style']."', '".$GLOBALS['zp_shortcode_attrs']['order']."', '".$GLOBALS['zp_shortcode_attrs']['sort']."', '".$GLOBALS['zp_shortcode_attrs']['year']."', '".$GLOBALS['zp_shortcode_attrs']['download']."', '".$GLOBALS['zp_shortcode_attrs']['author']."', '".$GLOBALS['zp_shortcode_attrs']['limit']."', '".$GLOBALS['zp_shortcode_attrs']['image']."', '".$GLOBALS['zp_instance_id']."', false);\n        ";
+                //echo "DisplayCitations(
+                //        '".$account->account_type."',
+                //        '".$account->api_user_id."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['data_type']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['collection_id']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['item_key']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['tag_name']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['content']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['style']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['order']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['sort']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['year']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['download']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['author']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['limit']."',
+                //        '".$GLOBALS['zp_shortcode_attrs']['image']."',
+                //        '".$GLOBALS['zp_instance_id']."',
+                //        false);";
+            }
+        }
+        
+        ?>
